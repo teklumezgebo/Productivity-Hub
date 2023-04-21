c9362a3b1f0582ebbfa49bec5b7bbb3ef01d1996
teklumezgebo <mezge002@umn.edu>
2023-04-20 20:54:14 -0500

    form submit function created

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,28 +1,36 @@
 import React from "react";
 
-function TaskForm({ taskName, dueDate, onTaskNameChange, onDueDateChange }) {
+function TaskForm({ taskName, dueDate, onListChange, onTaskNameChange, onDueDateChange }) {
     
-    // function handleFormSubmit() {
-    //     const newTask = {
-    //       task: taskName,
-    //       due: dueDate
-    //     }
+ const newTask = {
+  task: taskName,
+  due: dueDate
+ }
+
+ const postedTaskObj = {
+   method: 'POST',
+   headers: {'Content-Type': 'application/json'},
+   body: JSON.stringify(newTask)    
+ }
+
+ function handleFormSubmit() {
+    fetch('http://localhost:3000/tasks', postedTaskObj)
+    .then(res => res.json())
+    .then(task => onListChange(task))
+ }
     
-    //     fetch('http://localhost:3000/tasks', )
-    //   }
-    
-    return (
-        <div>
-            <form>
-                <label>New Task</label>
-                <input name="task" type="text" placeholder="Write your task here..." onChange={onTaskNameChange} value={taskName}></input>
-                <input name="due-by" type="text" placeholder="Due by..." onChange={onDueDateChange} value={dueDate}></input>
-                <select>
-                    <option>High Priority</option>
-                    <option>Low Priority</option>
-                </select>
-            </form>
-        </div>
+  return (
+    <div>
+        <form onSubmit={handleFormSubmit}>
+            <label>New Task</label>
+            <input name="task" type="text" placeholder="Write your task here..." onChange={onTaskNameChange} value={taskName}></input>
+            <input name="due-by" type="text" placeholder="Due by..." onChange={onDueDateChange} value={dueDate}></input>
+            <select>
+                <option>High Priority</option>
+                <option>Low Priority</option>
+            </select>
+        </form>
+    </div>
     )
 }
 
