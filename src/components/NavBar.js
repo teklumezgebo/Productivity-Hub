3bcc52fe089a63a1e94b58373e8870b8b1147366
teklumezgebo <mezge002@umn.edu>
2023-04-20 22:48:26 -0500

    navbar potential routes set up, done for the day

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,12 @@
 import React from "react";
 
-function NavBar() {
-    
-    function fetchAPI() {
-        fetch('http://localhost:3000/tasks')
-        .then(res => res.json())
-        .then(data => console.log(data))
-    }
-    
+function NavBar() {    
     return (
         <div>
-            <h3>Home</h3>
-            <h3>Place 1</h3>
-            <h3>Place 2</h3>
-            <button onClick={fetchAPI}>Toggle Mode</button>
+            <h3>To-Do List</h3>
+            <h3>Journal</h3>
+            <h3>Upcoming Events</h3>
+            <button>Toggle Mode</button>
         </div>
     )
 }
