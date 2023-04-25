a636c437c92e5b2530f34148ee377b49b15e8db2
teklumezgebo <mezge002@umn.edu>
2023-04-24 21:33:31 -0500

    nav bar change

--- src/components/EventCalender/EventList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function EventList() {
-    
-}
-
-export default EventList
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,9 @@ import React from "react";
 function NavBar() {    
     return (
         <div>
+            <h3>Home</h3>
             <h3>To-Do List</h3>
             <h3>Journal</h3>
-            <h3>Upcoming Events</h3>
         </div>
     )
 }
