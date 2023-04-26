87209133795677b2c27016d18ab42fd84f90f5a8
teklumezgebo <mezge002@umn.edu>
2023-04-25 22:01:23 -0500

    all components completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Home() {
+    const [time, setTime] = useState(new Date())
+
+    useEffect(() => {
+      
+        setInterval(() => {
+        setTime(new Date())
+      }, 1000)
+
+    }, [])
+    
     return(
         <div className="home">
-            <h1 className="title"></h1>
+            <h1 className="title">{time.toString()}</h1>
         </div>
     )
 }
